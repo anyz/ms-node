7d783139185300d42371b74814d25c8cef68b60e
anyz.khan@gmail.com <anyz.khan@gmail.com>
2021-03-08 11:53:33 -0600

    wip 2.0 - react-bootstrap enabled

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../app/layout/Header";
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 const Home = (props) => {
 
     return(
@@ -8,7 +12,13 @@ const Home = (props) => {
         <Header
             section="home">
         </Header>
-        <div>Welcome!!!</div>
+            <div>Welcome!!!</div>
+            <Navbar className="navbar navbar-inverse ms-header navbar-fixed-top">
+                <Nav className="ms-header">
+                    <Nav.Link href="#">Home</Nav.Link>
+                </Nav>
+            </Navbar>
+
     </div>
     );
 };
--- src/app/layout/Header.jsx
+++ src/app/layout/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import * as PropTypes from "prop-types";
-import NavLink from "./NavLink";
 import SubHeader from "./SubHeader";
+
+import {
+    Container,
+    Navbar,
+    Nav, Row, Col, Image
+    } from 'react-bootstrap';
 
 import { currentDate } from "../../app/Functions";
 
@@ -12,51 +17,56 @@ const Header = (props) => {
 
     return(
         <div>
-            <div id="header">
-                <header className="navbar navbar-inverse ms-header navbar-fixed-top">
-                    <div className="container">
-                        <div className="collapse navbar-collapse">
-                            <div className="row">
-                                <div className="col-md-4">
-                                    <div>{ currentDate() }</div>
-                                </div>
-                                <div className="col-md-4">
-                                    <img src="/images/header.png" height="35px" width="215px" alt="Home"/>
-                                </div>
-
-                                <ul className="nav navbar-nav navbar-right">
-                                    <li><span className="glyphicon glyphicon-home"></span> 14225 University Ave # 270, Waukee, IA 50263
-                                    </li>
-                                </ul>
-
-                            </div>
-                        </div>
-
-                        <div>
-                            <ul className="nav nav-pills nav-justified">
-                                <li>
-                                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"
-                                          className="donateForm">
-                                        <input type="hidden" name="cmd" value="_s-xclick"/>
-                                        <input type="hidden" name="hosted_button_id" value="NWWTRPY5NF8T8"/>
-                                        <input type="submit" name="submit" value="Donate Now!" alt="Donate Now"
-                                               className="btn btn-danger glow-button"/>
-                                    </form>
-
-                                </li>
-
-                                <NavLink title="Home" link="/home" active={section === "home"}/>
-                                <NavLink title="Programs" link="/programs" active={section === "programs"}/>
-                                <NavLink title="Prayer Times" link="/prayertimes" active={section === "prayertimes"}/>
-                                <NavLink title="Donations" link="/donations" active={section === "donations"}/>
-                                <NavLink title="Contact Us" link="/contactus" active={section === "contactus"}/>
-                            </ul>
-                        </div>
-
-
+        <div className="bg-dark navbar">
+            <Row>
+                <Col md={4}>
+                    <div className="white-text">{ currentDate() }</div>
+                </Col>
+                <Col md={4}>
+                    <Image src="/images/header.png" height="35px" width="215px" alt="Bismillah" />
+                </Col>
+                <Col md={4} >
+                    <div className="float-right white-text">
+                        14225 University Ave # 270, Waukee, IA 50263
                     </div>
-
-                </header>
+                </Col>
+            </Row>
+            </div>
+            <div id="header">
+                <div className="bg-dark navbar">
+                    <Container>
+                        <Row>
+                        <Col>
+                            <Nav fill>
+                                <Nav.Item className="nav-link">
+                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"
+                                      className="donateForm">
+                                    <input type="hidden" name="cmd" value="_s-xclick"/>
+                                    <input type="hidden" name="hosted_button_id" value="NWWTRPY5NF8T8"/>
+                                    <input type="submit" name="submit" value="Donate Now!" alt="Donate Now"
+                                           className="btn btn-danger glow-button"/>
+                                </form>
+                                </Nav.Item>
+                                <Nav.Item className="nav-link">
+                                    <Nav.Link href="/home">Home</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item className="nav-link">
+                                    <Nav.Link href="/programs">Programs</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item className="nav-link">
+                                    <Nav.Link href="/prayertimes">Prayer Times</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item className="nav-link">
+                                    <Nav.Link href="/donations">Donations</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item className="nav-link">
+                                    <Nav.Link href="/contactus">Contact Us</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        </Row>
+                    </Container>
+                </div>
             </div>
 
             <SubHeader />
