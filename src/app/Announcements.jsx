e7a698ddf87490204f94991ac1b0d42a47b72e4c
anyz <anyz.khan@gmail.comm>
2021-04-07 18:34:47 -0500

    adding monthly calander

--- src/app/Announcements.jsx
+++ src/app/Announcements.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Iqama from "./Iqama";
+import MonthlyTime from "./MonthlyTime";
 import iqamaTimes from '../.../../data/iqama.json';
 import { processIqama } from "../app/Functions";
 
 import { currentMonth } from "../app/Functions";
 
 import {
-    Table
+    Table, Accordion, Card, Button
     } from 'react-bootstrap';
 
 const Announcements = (props) => {
@@ -31,6 +32,21 @@ const Announcements = (props) => {
                             {itimes.map((itime, index) =>  <Iqama key={index} iqamaRow={itime}/> )}
                         </tbody>
                     </Table>
+
+                    <Accordion defaultActiveKey="0">
+                         <Card>
+                            <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                Monthly Calander
+                            </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <MonthlyTime />
+                            </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
 
                     <div id="twitterfeed">
                         <a className="twitter-timeline" data-width="50%" data-height="200px" data-chrome="nofooter noheader"
